742726f273379bc7d1129b5f34df423f3562bf3c
Irene Yoon <ey222@cornell.edu>
2016-11-09 16:17:43 -0500

    added basic server.ml

---
+++ _build/server.mli
@@ -0,0 +1,9 @@
+open Lwt
+open Cohttp
+open Cohttp_lwt_unix
+
+
+(**
+ * A simple cohttp server that outputs back request information.
+ *)
+let server = conn -> Cohttp.Request.t -> Cohttp_lwt_body.t -> (Cohttp.Response.t * Cohttp_lwt_body.t) Lwt.t
