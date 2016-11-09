a6c60f8c6da8463bcec694c2d72050241a9d7e70
Tyler Ishikawa <tyi3@cornell.edu>
2016-11-09 18:15:49 -0500

    update data.mli

--- data.mli
+++ data.mli
@@ -1,7 +1,15 @@
 open Yojson.Basic.Util
 
-type json = {
-  status_code: int;
-  (* TODO *)
+type server_json = {
+  day_count: int;
+  game_stage: string;
+  active_players: string list;
+  new_announcements: string list;
+  new_messages: string list;
 }
 
+type client_json = {
+  player_id: int;
+  player_action: string;
+  arguments: string list;
+}
