0bc59c410283ec8b54bbcdcd40f04f29aa1d3e2e
Tyler Ishikawa <tyi3@cornell.edu>
2016-11-09 22:36:59 -0500

    create encode and decode functions for json

--- client.mli
+++ client.mli
@@ -1,3 +1,5 @@
+open Data
+
 type game_state = {
   alive_players: string list;
   dead_players: string list;
@@ -5,13 +7,13 @@ type game_state = {
 
 (**
  * [get url f] will send a GET request to the URL specified by [url], parse the
- * resulting JSON into a data record, and pass the data into [f].
+ * resulting JSON into a client_json record, and pass it into [f].
  *)
-val get : string -> ( data -> unit ) -> unit
+val get : string -> ( client_json -> unit ) -> unit
 
 (**
- * [post url dat f] will convert [dat] into a JSON string and send it to the URL
- * specified by [url] as a POST request. The resulting JSON will be parsed into
- * a data record and passed into [f].
+ * [post url data f] will convert [data] into a JSON string and send it to the
+ * URL specified by [url] as a POST request. The resulting JSON will be parsed
+ * into a client_json record and passed into [f].
  *)
-val post : string -> data -> (data -> unit) -> unit
+val post : string -> client_json -> (client_json -> unit) -> unit
--- data.mli
+++ data.mli
@@ -13,3 +13,15 @@ type client_json = {
   player_action: string;
   arguments: string list;
 }
+
+(* Turns a server_json record into a JSON string *)
+val encode_sjson : server_json -> string
+
+(* Turns a client_json record into a JSON string *)
+val encode_cjson : client_json -> string
+
+(* Turns a JSON string into a server_json record *)
+val decode_sjson : string -> server_json
+
+(* Turns a JSON string into a client_json record *)
+val decode_cjson : string -> client_json
