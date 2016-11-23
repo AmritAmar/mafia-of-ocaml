32f0cdf07c46ef9b89ee0e402cb91a5fd84090fd
Tyler Ishikawa <tyi3@cornell.edu>
2016-11-23 15:26:04 -0500

    add gitignore and delete _build folder

--- _build/server.mli
+++
@@ -1,9 +0,0 @@
-open Lwt
-open Cohttp
-open Cohttp_lwt_unix
-
-	
-(**
- * A simple cohttp server that outputs back request information.
- *)
-val server : conn -> Cohttp.Request.t -> Cohttp_lwt_body.t -> (Cohttp.Response.t * Cohttp_lwt_body.t) Lwt.t
