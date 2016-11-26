7b66bd07644c03d4628863d7f96499883a15bd19
Michael Lucido <mjl329@cornell.edu>
2016-11-26 01:54:01 -0500

    Implement join_room?

--- game_server.ml
+++ game_server.ml
@@ -7,7 +7,7 @@ open Game
 open Ringbuffer
 
 type lobby_state = {
-    admin_name : string;
+    admin : string;
     players : (string * bool) list; 
 }
 
@@ -16,7 +16,8 @@ type server_state =
    | Game of game_state  
 
 type room_data = {
-    mutable state: server_state; 
+    state: server_state; 
+    (* TODO: Write Immutable ArrayBuffer? (gah) *)
     chat_buffer: (timestamp * chat_message) ArrayBuffer.t;
     action_buffer: client_json ArrayBuffer.t; 
 }
@@ -33,35 +34,68 @@ let extract_id req =
     let uri = Cohttp.Request.uri req in 
     Uri.get_query_param uri "room_id"
 
+let room_op req op =
+    let id = extract_id req in 
+    match id with 
+        | None -> 
+            Server.respond_with_string ~code: `Bad_request "Malformed room_id."
+        | Some s when not (Hashtbl.mem rooms s) -> 
+            Server.respond_with_string ~code: `Bad_request "Room doesn't exist."
+        | Some s ->
+            let room_data = Hashtbl.find_exn rooms s in op s room_data 
+
 let create_room conn req body = 
     let id = extract_id req in
     match id with 
         | None -> 
-            Server.respond_with_string ~code:`Bad_request "Invalid room_id" 
+            Server.respond_with_string ~code:`Bad_request "Malformed room_id" 
         | Some s when Hashtbl.mem rooms s -> 
             Server.respond_with_string ~code:`Conflict "Room already exists." 
         | Some s -> 
             let room = {
-                state = Lobby {admin_name = ""; players = []};
+                state = Lobby {admin = ""; players = []};
                 chat_buffer = ArrayBuffer.make 3110;
                 action_buffer = ArrayBuffer.make 3110; 
             } in 
             Hashtbl.replace rooms s room; 
             Server.respond_with_string  ~code:`Created "Room created."
 
+
 let join_room conn req body = 
-    (*
-        extract the room id 
-        if the room exists
-            is the user first?
-                give them admin rights
-            get their name 
-            return ok 
-        else
-            return a 400 error
-    *)
-    
-    failwith "unimplemented"
+
+    let add_player s l =  
+        let in_use = List.fold ~init:true ~f:(fun acc (n,_) -> (n = s) && acc) l.players in 
+        let too_long = String.length s >= 20 in 
+
+        if (in_use || too_long) then None 
+        else if l.players = [] then 
+            Some {l with admin = s; players = [(s,true)]}
+        else 
+            Some {l with players = (s,false)::l.players}
+    in 
+
+    let join body = 
+        try 
+            let cd = decode_cjson body in 
+            let lobby_op id rd = 
+                match rd.state with 
+                    | Game _ ->
+                        Server.respond_with_string ~code: `Bad_request "Game already in progress."
+                    | Lobby l ->
+                    let result = add_player (cd.player_id) (l) in 
+                    (match result with 
+                        | None -> 
+                            Server.respond_with_string ~code: `Conflict "player_id in use."
+                        | Some l' ->
+                            Hashtbl.replace rooms id {rd with state = Lobby l'}; 
+                            Server.respond_with_string ~code: `OK "Joined!")  
+            in 
+            room_op (req) (lobby_op)
+        with _ -> Server.respond_with_string ~code: `Bad_request "Malformed client_action.json"
+
+    in 
+
+    Body.to_string body >>= join 
 
 let player_action conn req body = 
     failwith "unimplemented"
