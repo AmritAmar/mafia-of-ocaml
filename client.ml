2ad6ae765745933ecf1d064cc0a2063a26d0b0dc
Tyler Ishikawa <tyi3@cornell.edu>
2016-11-26 01:51:34 -0500

    create make_uri function and begin main REPL

--- client.ml
+++ client.ml
@@ -5,9 +5,14 @@ open Core.Std
 open Cohttp
 open Cohttp_async
 
-let send_get url f =
+let make_uri base_url path query_params =
+  let new_uri = Uri.of_string base_url in
+  let new_uri = Uri.with_path new_uri path in
+  Uri.add_query_params' new_uri query_params
+
+let send_get uri f =
   let code_and_json =
-    Client.get (Uri.of_string url) >>= fun (resp,body) ->
+    Client.get uri >>= fun (resp,body) ->
     let code = resp |> Response.status |> Code.code_of_status in
     let new_ivar = Ivar.create () in
     upon (Body.to_string body)
@@ -16,10 +21,9 @@ let send_get url f =
   in
   upon code_and_json (fun (code,sjson) -> f code sjson)
 
-let send_post url data f =
+let send_post uri data f =
   let code_and_json =
-    Client.post (Uri.of_string url) ~body:(`String (encode_cjson data))
-    >>= fun (resp,body) ->
+    Client.post uri ~body:(`String (encode_cjson data)) >>= fun (resp,body) ->
     let code = resp |> Response.status |> Code.code_of_status in
     let new_ivar = Ivar.create () in
     upon (Body.to_string body)
@@ -30,13 +34,18 @@ let send_post url data f =
 
 (* Main REPL *)
 let _ =
-  let rec reprompt f err_msg =
+  let rec get_input () =
     new_prompt ();
     let input = read_line () in
-    if f input then input
-    else (update_announcements [err_msg]; reprompt f err_msg)
+    if input = "" then get_input ()
+    else input
   in
-  update_announcements ["Welcome to mafia_of_ocaml! Please enter a username.";
-                        "The username should be <= 20 characters long."];
-  reprompt (fun s -> String.length s <= 20)
-           "Please enter a username that is <= 20 characters long."
+  if Array.length Sys.argv < 2
+  then (print_endline "Usage: make client URL=[server URL]"; exit 0);
+  (*update_announcements*)
+  Printf.printf "%s\n" "Welcome to mafia_of_ocaml! Please enter a username that is <= 20 characters long.";
+  let user = get_input () in
+  (*update_announcements*)
+  Printf.printf "%s\n" "Type \"join [room_id]\" to join an existing room or \"create [room_id]\" to create a new room.";
+  let room = get_input () in
+  Printf.printf "%s\t%s\n" user room
