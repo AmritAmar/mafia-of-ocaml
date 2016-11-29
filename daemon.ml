47e550c4e3383421219e2b1a060b667420406750
Michael Lucido <mjl329@cornell.edu>
2016-11-29 01:52:01 -0500

    Convince Daemon to Work Properly

--- daemon.ml
+++ daemon.ml
@@ -4,11 +4,11 @@ open Async.Std
 open Cohttp
 open Cohttp_async
 
-let url = "localhost:3110/daemon"
+let url = "http://localhost:3110/daemon"
 
 let rec loop () =
   let helper =
-    Client.post (Uri.of_string url) >>= fun _ ->
+    Cohttp_async.Client.post (Uri.of_string url) >>= fun _ ->
     after (Core.Std.sec 5.0)
   in
     upon helper (fun _ -> loop ())
