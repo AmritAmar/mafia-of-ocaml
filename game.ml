567cee04af4a3ec49e0546629cc9ec162057dfbe
sk2472 <sk2472@cornell.edu>
2016-12-05 00:25:44 -0500

    wording changes to announcements

--- game.ml
+++ game.ml
@@ -6,8 +6,8 @@ open Core
 type player_name = string
 type chat_message = string
 
-type target = All | Innocents | Mafias | Player of player_name 
-type announcement = target * string 
+type target = All | Innocents | Mafias | Player of player_name
+type announcement = target * string
 
 type timestamp = Core.Time.t
 type role = Innocent | Mafia | Dead
@@ -25,23 +25,23 @@ type game_state = {
  * Assign roles to players
  * 1/3 of the players become a member of the Mafia
  *)
-let rec assign_roles counter assigned players num_players = 
-    match players with 
+let rec assign_roles counter assigned players num_players =
+    match players with
     [] -> assigned
-    | h::t -> if (counter >= num_players/3) then 
-        assign_roles (counter+1) ((h,Innocent)::assigned) t num_players 
+    | h::t -> if (counter >= num_players/3) then
+        assign_roles (counter+1) ((h,Innocent)::assigned) t num_players
     else assign_roles (counter+1) ((h,Mafia)::assigned) t num_players
 
 let rec get_mafia players mafia = match players with
     [] -> mafia
     | (x,y)::t -> if (y=Mafia)
-                    then get_mafia t (x^", "^mafia) 
+                    then get_mafia t (x^", "^mafia)
                     else get_mafia t mafia
 
 let check old_st st =
-    let check_victory st role = 
-        let check acc (_,x) = (x = Dead || x = role) && acc in 
-        List.fold_left check true st.players 
+    let check_victory st role =
+        let check acc (_,x) = (x = Dead || x = role) && acc in
+        List.fold_left check true st.players
     in
 
     if (check_victory st Innocent) then
@@ -49,27 +49,27 @@ let check old_st st =
                  announcement_history = (Time.now (), (All,
                  "Congratulations! The Innocents have won."))
                  ::old_st.announcement_history}
-    else if (check_victory st Mafia) then 
-        {old_st with stage = Game_Over; 
+    else if (check_victory st Mafia) then
+        {old_st with stage = Game_Over;
                  announcement_history = (Time.now (), (All,
-                 "Congratulations! The Mafias have won."))
-                 ::old_st.announcement_history} 
+                 "Congratulations! The Mafia has won."))
+                 ::old_st.announcement_history}
     else st
 (*
  * Assumes j is list of players, 1/4 of players becomes mafia
  *)
-let init_state lst = 
+let init_state lst =
     let pl =  assign_roles 0 [] lst (List.length lst) in
     {day_count = 0; stage = Discussion;
-        players = pl; 
-        announcement_history = [(Time.now (), 
-            (Innocents, "You are an Innocent citizen of the town,"^
+        players = pl;
+        announcement_history = [(Time.now (),
+            (Innocents, "You are an Innocent citizen of the town, "^
                 "and must find out and execute all the Mafia to survive!"));
-        (Time.now (), 
-            (Mafias, "You are a member of the Mafia."^
-                "Try to kill all of the innocent citizens without getting"^
-                " found and executed! These are your fellow mafia members: "^
-                (get_mafia pl "")))]}
+        (Time.now (),
+            (Mafias, "You are a member of the Mafia. "^
+            "Try to kill all of the innocent citizens without getting "^
+            "found and executed! These are your fellow members of the mafia: "^
+            (get_mafia pl "")))]}
 
 let kill_player p pl =
     List.map (fun (x,y) -> if x=p then (x,Dead) else (x,y)) pl
@@ -80,51 +80,51 @@ let kill_player p pl =
  * If there is a tie, returns one of the players.
  *)
 let handle_exec_vote (st:game_state) (players:player_name list)  =
-    let now = Time.now () in 
-    let p = List.sort String.compare players in 
-    
-    let rec voting (player:string) (acc:int) (pl: player_name list) = 
+    let now = Time.now () in
+    let p = List.sort String.compare players in
+
+    let rec voting (player:string) (acc:int) (pl: player_name list) =
         match pl with
             | [] -> ""
             | h::t -> if h = player then (if (acc+1) >= (List.length p)/2
                   then h else voting player (acc+1) t)
-                  else voting h 1 t 
+                  else voting h 1 t
     in
-    
+
     let voted = voting "" 0 p in
-    
-    if voted = "" then 
+
+    if voted = "" then
         let a = (All, "No majority vote has occured. "^
-                      "No one is being executed.") in 
+                      "No one will be executed.") in
         {st with announcement_history = (now,a) :: st.announcement_history}
-    else 
-    
+    else
+
     let voted_player = List.filter (fun (x,_) -> x <> voted) st.players in
     let (_,voted_role) = List.hd voted_player in
     let a = if voted_role = Innocent then
-                (All, 
+                (All,
                 "Sadly, "^voted^" was voted guilty and has been executed.\n" ^
                  voted ^" was an Innocent citizen.")
-            else 
+            else
                 (All,
                 voted ^ " was voted guilty and has been executed.\n" ^
-                voted ^ " was a Mafia! Nice work!")
-    in 
-    {st with players = kill_player voted st.players; 
-             announcement_history = (now, (Player voted, 
-                "You were voted as guilty and was executed. :("))::(now,a):: 
+                voted ^ " was a member of the Mafia! Nice work!")
+    in
+    {st with players = kill_player voted st.players;
+             announcement_history = (now, (Player voted,
+                "You were voted as guilty and was executed. :("))::(now,a)::
                 st.announcement_history}
 (*
  * Checks if the game has ended:
  * Game ends if either - everyone is innocent or everyone is mafia
  *)
-let end_check (players : (player_name * role) list) = 
+let end_check (players : (player_name * role) list) =
     (List.fold_left (fun a (_,x)-> (x = Mafia) || a) false players) <>
     (List.fold_left (fun a (_,x)-> (x = Innocent) || a) false players)
 
 (** returns whether or not player is a mafia
  *)
-let is_mafia player state = 
+let is_mafia player state =
     List.assoc player state.players = Mafia
 
 (** returns whether or not player is alive
@@ -136,25 +136,25 @@ let is_alive player state =
  * Gets latest votes given a list of votes
  * Assumes that later votes are closer to the tail of the list
  *)
-let rec latest_votes latest votes = 
+let rec latest_votes latest votes =
     match votes with
     [] -> latest
-    | hd::tl -> let new_list = List.filter 
+    | hd::tl -> let new_list = List.filter
             (fun x -> x.player_id <> hd.player_id) latest in
                 latest_votes (hd::new_list) tl
 
 (**
  * Returns the most voted person *)
-let rec most_voted max_vote max_count vote count votes = 
-    match votes with 
+let rec most_voted max_vote max_count vote count votes =
+    match votes with
     [] -> if max_count < count then vote else max_vote
     | hd::tl -> if vote = hd then most_voted max_vote max_count hd (count+1) tl
         else (if count > max_count then most_voted vote count hd 1 tl
         else most_voted max_vote max_count hd 1 tl)
 
-let night_to_disc st updates = 
+let night_to_disc st updates =
     (* Only adds to list_killed if player is mafia*)
-    let victim_list = List.fold_left 
+    let victim_list = List.fold_left
             (fun a x -> if(is_mafia x.player_id st && x.player_action = "vote")
              then x::a else a)
             [] updates |> latest_votes [] |> List.map (fun x -> x.arguments)
@@ -162,10 +162,10 @@ let night_to_disc st updates =
     let victim = match victim_list with [] -> ""
     | hd::tl -> most_voted hd 1 hd 1 tl in
     let updated_players = kill_player victim st.players in
-     
-    {day_count = st.day_count+1; stage = Discussion; 
-        players = updated_players; 
-        announcement_history = (Time.now (), (All, 
+
+    {day_count = st.day_count+1; stage = Discussion;
+        players = updated_players;
+        announcement_history = (Time.now (), (All,
              "Good Morning! Last night, "^victim^
              " was killed in their sleep by the Mafia :( RIP."))
              ::st.announcement_history}
@@ -173,30 +173,30 @@ let night_to_disc st updates =
 (*
  * Assumes it only receives chats during disc
  *)
-let disc_to_voting st updates = 
-    {st with stage = Voting; 
+let disc_to_voting st updates =
+    {st with stage = Voting;
              announcement_history = (Time.now (), (All,
                 "Discussion time is now over. \n"^
-                "Please vote on a player that could "^
-                "be a member of the Mafia."))
+                "Please vote on a player that you think "^
+                "is a member of the Mafia."))
              ::st.announcement_history}
 
 (*
  * Latest votes are taken account, each player's votes only count once
  *)
-let voting_to_night st updates = 
-    let s = List.fold_left 
+let voting_to_night st updates =
+    let s = List.fold_left
             (fun a x -> if(x.player_action = "vote") then x::a else a)
             [] updates |> latest_votes [] |> List.map (fun x -> x.arguments)
-            |> List.concat |> handle_exec_vote st 
+            |> List.concat |> handle_exec_vote st
          in
-    {s with stage = Night; 
+    {s with stage = Night;
              announcement_history = (Time.now (), (All,
              "Its night time now - go sleep unless you "^
              "have someone to visit :)"))
              ::s.announcement_history}
 
-let string_of_stage = function 
+let string_of_stage = function
     | Night -> "Night"
     | Discussion -> "Discussion"
     | Voting -> "Voting"
@@ -204,34 +204,34 @@ let string_of_stage = function
 
 let can_chat state =
     match state.stage with
-     | Night -> false 
+     | Night -> false
      | Discussion | Voting | Game_Over -> true
 
-let can_vote state player = 
+let can_vote state player =
     match state.stage with
     | Voting -> is_alive player state
     | Night -> is_mafia player state
     | _ -> false
 
-let can_recieve state player target = 
-    match target with 
-        | All -> true 
+let can_recieve state player target =
+    match target with
+        | All -> true
         | Innocents -> not (is_mafia player state)
-        | Mafias -> is_mafia player state 
-        | Player s -> player = s 
+        | Mafias -> is_mafia player state
+        | Player s -> player = s
 
 (** [disconnect_player] disconencts player given game state and player name
  *)
 let disconnect_player state player =
-    {day_count = state.day_count; stage = state.stage; 
-        players = kill_player player state.players; 
+    {day_count = state.day_count; stage = state.stage;
+        players = kill_player player state.players;
         announcement_history = (Time.now (), (All,
              "Player " ^ player ^ " has disconnected."))
              ::state.announcement_history}
 
 (** [time_span] returns the appropriate Time.span according to given state
  *)
-let time_span state = 
+let time_span state =
     match state.stage with
     | Voting -> Core.Time.Span.of_sec 30.
     | Game_Over -> Core.Time.Span.of_sec 30.
@@ -239,12 +239,12 @@ let time_span state =
     | Night -> Core.Time.Span.minute
 
 
-let step_game st updates = 
-    
+let step_game st updates =
+
     (* maybe not most fluent game play if end check is here *)
 
-        match st.stage with 
+        match st.stage with
             | Night -> night_to_disc st updates |> check st
             | Discussion -> disc_to_voting st updates |> check st
             | Voting -> voting_to_night st updates |> check st
-            | Game_Over -> st 
+            | Game_Over -> st
