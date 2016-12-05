cf94e07f80c41acf27b3e0291906d9adfe24e8c9
Irene Yoon <ey222@cornell.edu>
2016-12-05 03:00:22 -0500

    tab

--- test_game.ml
+++ test_game.ml
@@ -27,45 +27,45 @@ let vote_kill_irene3 = handle_exec_vote init_state ["Irene";
   "Michael";"Michael";"Irene"]
 let client_json1 = [{player_id = "Irene";player_action = "vote";
   arguments = ["Michael"]};
-					{player_id = "Michael";player_action = "vote";
+          {player_id = "Michael";player_action = "vote";
             arguments = ["Michael"]};
-					{player_id = "Tyler";player_action = "vote";
+          {player_id = "Tyler";player_action = "vote";
             arguments = ["Tyler"]};
-					{player_id = "Rachel";player_action = "vote";
+          {player_id = "Rachel";player_action = "vote";
             arguments = ["Tyler"]};
-					{player_id = "Irene";player_action = "vote";
+          {player_id = "Irene";player_action = "vote";
             arguments = ["Tyler"]};
-					{player_id = "Rachel";player_action = "vote";
+          {player_id = "Rachel";player_action = "vote";
             arguments = ["Tyler"]}]
 let latest_client_json1 = 
-					[{player_id = "Rachel";player_action = "vote";
+          [{player_id = "Rachel";player_action = "vote";
             arguments = ["Tyler"]};
-					{player_id = "Irene";player_action = "vote";
+          {player_id = "Irene";player_action = "vote";
             arguments = ["Tyler"]};
-					{player_id = "Tyler";player_action = "vote";
+          {player_id = "Tyler";player_action = "vote";
             arguments = ["Tyler"]};
-					{player_id = "Michael";player_action = "vote";
+          {player_id = "Michael";player_action = "vote";
             arguments = ["Michael"]}
-					]
+          ]
 let vote_no_majority = handle_exec_vote init_state ["Irene";
   "Tyler";"Michael";"Rachel"]
 
 
 let game_tests = [
   "assign_roles" >:: (fun _ -> assert_equal 
-  	plr_lst
+    plr_lst
     (assign_roles 0 [] id_lst (List.length id_lst)));
   "kill_player" >:: (fun _ -> assert_equal kill_irene
     (kill_player "Irene" plr_lst));
   "handle_exec_vote_no_m" >:: (fun _ -> assert_equal plr_lst
     (vote_no_majority.players));
   "handle_exec_vote_inno1" >:: (fun _ -> assert_equal kill_irene
-  	(vote_kill_irene.players));
+    (vote_kill_irene.players));
    "handle_exec_vote_inno3" >:: (fun _ -> assert_equal kill_irene
-  	(vote_kill_irene2.players));
+    (vote_kill_irene2.players));
 
   "handle_exec_vote_amb" >:: (fun _ -> assert_equal kill_irene
-  	(vote_kill_irene3.players)); (* ambiguous!!! *)
+    (vote_kill_irene3.players)); (* ambiguous!!! *)
   "latest_votes" >:: (fun _ -> assert_equal latest_client_json1
     ((latest_votes [] client_json1)));
   "night_to_disc" >:: (fun _ -> assert_equal (state2.day_count)
